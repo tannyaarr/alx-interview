b084ffb849b8dc52be7572ba2fce09fe9bbfb522
tannyaarr <tanyachiras@gmail.com>
2024-04-27 18:39:44 +0200

    returns a list of lists of integers representing the pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,15 +3,15 @@
 
 
 def pascal_triangle(n):
+    """Defines a pascal triangle"""
     if n <= 0:
         return []
-
-    triangle = [[18]]
+    triangle = [[1]]
     for i in range(1, n):
-        row = [1]
+        prev_row = triangle[-1]
+        new_row = [1]
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
-
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        new_row.append(1)
+        triangle.append(new_row)
     return triangle
