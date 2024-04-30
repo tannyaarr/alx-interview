b8afb9943e1146e3672894dfd731dd275a98d0e4
tannyaarr <tanyachiras@gmail.com>
2024-04-30 11:35:34 +0200

    methood that determines if all boxes can be opened

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Method that determines if all boxes can be opened"""
+
+def canUnlockAll(boxes):
+    """the initialization of canUnlockAll"""
+    if not boxes:
+        return False
+
+    n = len(boxes)
+    visited = set()
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        if box not in visited:
+            visited.add(box)
+            for key in boxes[box]:
+                if 0 <= key < n:
+                    stack.append(key)
+
+    return len(visited) == n
