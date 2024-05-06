712f866025d9a4344bb311ab61465a5564543e43
tannyaarr <tanyachiras@gmail.com>
2024-05-06 17:18:54 +0200

    number of operations needed to result in exactly n H characters in the file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/ bin/python3
+"""Method that calculates the fewest number of operations needed to result in 
+exactly n H characters in the file"""
+
+
+def minOperations(n):
+    if n == 1:
+        return 0
+    
+    operations = 0
+    clipboard = 1
+    count = 1
+    
+    while count < n:
+        if n % count == 0:
+            operations += 2  
+            clipboard = count
+            count *= 2
+        else:
+            operations += 1
+            count += clipboard
+    
+    return operations
