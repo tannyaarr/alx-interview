21c0afc4b238556f41758cc0eec28faded678895
tannyaarr <tanyachiras@gmail.com>
2024-05-07 12:40:39 +0200

    fixed pycodestyle issues and indentation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""Method that calculates the fewest number of operations needed to result in 
+"""Method that calculates the fewest number of operations needed to result in
 exactly n H characters in the file"""
 
 
 def minOperations(n):
+    """Calculates the fewest number """
     if n == 1:
         return 0
-    
+
     operations = 0
     clipboard = 1
     count = 1
-    
+
     while count < n:
         if n % count == 0:
-            operations += 2  
+            operations += 2
             clipboard = count
             count *= 2
         else:
             operations += 1
             count += clipboard
-    
-    return operations
+
+            return operations
