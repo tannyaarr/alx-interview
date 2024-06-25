488fe4edf3ee295fd9c808971bf854f6a88f8a36
tannyaarr <tanyachiras@gmail.com>
2024-06-25 20:31:47 +0200

    returns the perimeter of the island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""returns the perimete of the island"""
+"""returns the perimeter of the island"""
 
 
 def island_perimeter(grid):
-    if not grid or not grid[0]:
-        return 0
-
     rows = len(grid)
     cols = len(grid[0])
-    perimeter = 0
-
+    land = 0
+    shared_edges = 0
+    
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
-                if r == 0 or grid[r-1][c] == 0:  
-                    perimeter += 1
-                if r == rows-1 or grid[r+1][c] == 0:
-                    perimeter += 1
-                if c == 0 or grid[r][c-1] == 0:
-                    perimeter += 1
-                if c == cols-1 or grid[r][c+1] == 0:
-                    perimeter += 1
-
-    return perimeter
+                land += 1
+                if r < rows - 1 and grid[r + 1][c] == 1:
+                    shared_edges += 1
+                if c < cols - 1 and grid[r][c + 1] == 1:
+                    shared_edges += 1
+    
+    return 4 * land - 2 * shared_edges
