9b3bcfa6782661ccf1ae62989589ea3c6e970a8e
tannyaarr <tanyachiras@gmail.com>
2024-06-25 20:43:02 +0200

    returns the perimeter of the island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,9 +14,9 @@ def island_perimeter(grid):
         for c in range(cols):
             if grid[r][c] == 1:
                 perimeter += 4
-                if r < rows - 1 and grid[r + 1][c] == 1:
+                if r > 0 and grid[r-1][c] == 1:
                     perimeter -= 2
-                if c < cols - 1 and grid[r][c + 1] == 1:
+                if c > 0 and grid[r][c-1] == 1:
                     perimeter -= 2
                     
     return perimeter
